8ed0fe56183f9d4b81a8462264046deb0dc1b071
Mikhail Sulaev <49402569+msulaev@users.noreply.github.com>
2024-09-16 09:29:06 +0300

    Use PageManager instead classes

--- tests/article.spec.ts
+++ tests/article.spec.ts
@@ -1,9 +1,7 @@
 import { faker } from "@faker-js/faker";
 import { DEFAULT_USER } from "../helpers/constants";
-import {LoginPage} from "../pages/LoginPage";
-import {EditorPage} from "../pages/EditorPage";
-import {ArticlePage} from "../pages/ArticlePage";
 import {expect, test} from "@playwright/test";
+import {PageManager} from "../pages/PageManager";
 
 const ARTICLE = {
     title: faker.lorem.words(),
@@ -15,50 +13,48 @@ const ARTICLE_URL = '#/article/tot-patruus-alo';
 
 test.describe('Article', () => {
     test.beforeEach(async ({page}) => {
-        let loginPage = new LoginPage(page);
-        await loginPage.visit();
-        await loginPage.login(DEFAULT_USER.email, DEFAULT_USER.password);
-        await expect(loginPage.navigation).toContainText(DEFAULT_USER.name);
+        let pm = new PageManager(page);
+        await pm.loginPage.visit();
+        await pm.loginPage.login(DEFAULT_USER.email, DEFAULT_USER.password);
+        await expect(pm.loginPage.navigation).toContainText(DEFAULT_USER.name);
     });
 
     test('User could publish article', async ({page}) => {
-        let editorPage = new EditorPage(page);
-        let articlePage = new ArticlePage(page);
-        await editorPage.clickOnNewArticle();
-        await editorPage.createArticle(ARTICLE.title, ARTICLE.description, ARTICLE.body);
-        await editorPage.clickOnPublish();
-        await expect(articlePage.article).toHaveText(ARTICLE.title);
+        let pm = new PageManager(page);
+        await pm.editorPage.clickOnNewArticle();
+        await pm.editorPage.createArticle(ARTICLE.title, ARTICLE.description, ARTICLE.body);
+        await pm.editorPage.clickOnPublish();
+        await expect(pm.articlePage.article).toHaveText(ARTICLE.title);
     });
 
     test('User could not publish article with existing title', async ({page}) => {
-        let editorPage = new EditorPage(page);
-        await editorPage.clickOnNewArticle();
-        await editorPage.createArticle('test', ARTICLE.description, ARTICLE.body);
-        await editorPage.clickOnPublish();
-        await expect(editorPage.errorMsg).toHaveText(ERROR);
+        let pm = new PageManager(page);
+        await pm.editorPage.clickOnNewArticle();
+        await pm.editorPage.createArticle('test', ARTICLE.description, ARTICLE.body);
+        await pm.editorPage.clickOnPublish();
+        await expect(pm.editorPage.errorMsg).toHaveText(ERROR);
     });
 
     test('User could not publish article with empty fields', async ({page}) => {
-        let editorPage = new EditorPage(page);
-        await editorPage.clickOnNewArticle();
-        await editorPage.clickOnPublish();
-        await expect(editorPage.titleInp).toBeVisible();
+        let pm = new PageManager(page);
+        await pm.editorPage.clickOnNewArticle();
+        await pm.editorPage.clickOnPublish();
+        await expect(pm.editorPage.titleInp).toBeVisible();
     });
 
     test('User could add comment', async ({page}) => {
-        let articlePage = new ArticlePage(page);
+        let pm = new PageManager(page);
         let msg = faker.lorem.words();
-        await articlePage.visitUrl(ARTICLE_URL);
-        await articlePage.writeCommentAndPost(msg);
+        await pm.articlePage.visitUrl(ARTICLE_URL);
+        await pm.articlePage.writeCommentAndPost(msg);
         await expect(page.getByText(msg, {exact: true})).toHaveText(msg);
     });
 
     test('User could delete comment', async ({page}) => {
-        let articlePage = new ArticlePage(page);
-        await articlePage.visitUrl('#/article/alter-test');
-        await articlePage.writeCommentAndPost('test_delete');
-        await articlePage.deleteComment();
-        await expect(articlePage.cards).toHaveCount(1);
-
+        let pm = new PageManager(page);
+        await pm.articlePage.visitUrl('#/article/alter-test');
+        await pm.articlePage.writeCommentAndPost('test_delete');
+        await pm.articlePage.deleteComment();
+        await expect(pm.articlePage.cards).toHaveCount(1);
     });
 });
